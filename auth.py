683fc3c7e6eb767e8fcc3a9067da173a72f94f78
phathway <22664626+pamelahathway@users.noreply.github.com>
2023-08-28 11:56:14 +0300

    store db as json

--- auth.py
+++ auth.py
@@ -1,4 +1,18 @@
+import json
+
 def get_credentials():
     username = input("Username: ")
     password = input("Password: ")
     return username, password
+
+
+def write_pwdb(username, password):
+    pwdb = {username: password}
+    path = "./pwdb.json"
+    with open(path, "w") as f:
+        json.dump(pwdb, f)
+
+
+username, password = get_credentials()
+write_pwdb(username, password)
+
